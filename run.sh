86a25cb9ddc0cab2f80e7f10b0c7dbce6069f26e
Michal Cesek <cesek.michal@gmail.com>
2017-12-09 00:20:31 +0100

    Move dep. installation to setup.sh script

--- run.sh
+++ run.sh
@@ -4,11 +4,4 @@ set -a
 . ./.env
 set +a
 
-apt update
-apt install python-pip -y
-pip install virtualenv
-virtualenv -p python3 python3_venv
-source python3_venv/bin/activate
-pip install --trusted-host pypi.python.org -r requirements.txt
-
 python3 monitor.py >> log.txt
---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+apt update
+apt install python-pip -y
+pip install virtualenv
+virtualenv -p python3 python3_venv
+source python3_venv/bin/activate
+pip install --trusted-host pypi.python.org -r requirements.txt
+
